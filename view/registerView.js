9bb16659e48eefcb695c2dfcd3e8fbe32a99ce51
dharaneesh-r <dharaneesh@tenext.in>
2025-12-31 18:48:45 +0530

    Added the loggers info in the registerView

--- view/registerView.js
+++ view/registerView.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const PostRegister = require("../controller/registerController");
+const logger = require("../config/logger");
 const app = express();
 
 // ROUTER CONFIGURATION
@@ -15,5 +16,8 @@ app.use(express.json());
 // ROUTES INTEGRATION
 
 RegisterRouter.post("/", PostRegister);
+
+
+logger.info("RegisterRouter", RegisterRouter)
 
 module.exports = RegisterRouter;
